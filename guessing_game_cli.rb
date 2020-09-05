3e3f34b0b486fad405e97432e9f4c8f71ecf2bf6
Lauren Cunningham <lauren.busbee@gmail.com>
2020-09-05 20:32:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,6 +9,6 @@ def run_guessing_game
   if user_input == "exit"
     puts "Goodbye"
   end 
-  if puts "Sorry! The computer guessed #{x}."
+  else puts "Sorry! The computer guessed #{x}."
   end
 end
